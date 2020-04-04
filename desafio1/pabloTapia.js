a1899a0936b28ed1a91f5e3ed24c9e3c554bd517
Pablo Tapia <ptapia@proyecto404.com>
2020-04-04 17:38:48 -0300

    mejoras de codigo

--- desafio1/pabloTapia.js
+++ desafio1/pabloTapia.js
@@ -1,27 +1,32 @@
 let data = require('./data');
-normalizeString();
-let totalSeconds = data.split('</li>')
-    .filter(areFlexboxVideo)
-    .map(mapStringForMinutesAnsSeconds)
+console.log(data)
+normalizeData();
+const totalSeconds = dataToArray()
+    .filter(isFlexboxVideo)
+    .map(getminutesAndSecondFromString)
     .map(minutesAndSecondsToTotalSeconds)
     .reduce((total, num) => total += num)
 
-console.log(`Segundos totales ${totalSeconds}`);
+console.log(`Segundos totales: ${totalSeconds}`);
 
-function areFlexboxVideo(value) {
+function dataToArray() {
+    return data.split('</li>');
+}
+
+function isFlexboxVideo(value) {
     return value.includes('flexbox');
 }
 
 function minutesAndSecondsToTotalSeconds(value) {
-    let minutesSeconds = value.split(":");
-    return parseInt(minutesSeconds[0], 10) * 60 + parseInt(minutesSeconds[1], 10);
+    let [minutes, seconds] = value.split(":").map(value => parseInt(value, 10))
+    return minutes * 60 + seconds;
 }
 
-function mapStringForMinutesAnsSeconds(value) {
+function getminutesAndSecondFromString(value) {
     return value.substring(value.indexOf("\"") + 1, value.lastIndexOf("\""));
 }
 
-function normalizeString() {
+function normalizeData() {
     data = data.replace('<ul>', '');
     data = data.replace('</ul>', '');
     data = data.toLowerCase();
